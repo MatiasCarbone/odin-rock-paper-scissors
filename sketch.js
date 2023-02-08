acf150fa70a158c23c36376b6f84d62d7b6cf222
Matías Carbone <matiascarbone2@gmail.com>
2023-02-07 23:43:49 -0300

    Add function that generates random computer choice

--- sketch.js
+++ sketch.js
@@ -1,1 +1,17 @@
-alert("Javascript sketch was linked successfully!");
+// alert("Javascript sketch was linked successfully!");
+
+function getComputerChoice() {
+  max = 3;
+  min = 1;
+
+  choice = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  switch (choice) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+}
